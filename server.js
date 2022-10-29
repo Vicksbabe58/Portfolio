571d661f4ac888dd54b61884721507ee79c4fcba
victory <victoryilikah58@gmail.com>
2022-10-29 12:33:17 +0100

    Made some correction

--- server.js
+++ server.js
@@ -2,14 +2,12 @@
 let http = require("http");
 let fs = require("fs");
 let path = require("path");
-let url = require("url");
 
 // create server with http
 let server = http.createServer((req, res) => {
   if (req.url === "/") {
     let filePath = path.join(__dirname, "index.html");
     fs.readFile(filePath, "utf8", (err, data) => {
-      //  fs.readFile("./index.html", (err, data) => {
       if (err) throw err;
       res.writeHead(200, {
         "Content-Type": "text/html",
